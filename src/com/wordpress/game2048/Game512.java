64e0622d40b1c20aa76deafe3e4d9091105399ae
caoscott <scottcao@outlook.com>
2016-10-23 21:00:35 -0500

    Reformatted code.

---
+++ src/com/wordpress/game2048/Game512.java
@@ -0,0 +1,46 @@
+package com.wordpress.game2048;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by scott on 10/23/16.
+ */ //multi-player
+class Game512 extends JPanel {
+
+	private GameBoard512 panel1;
+	private GameBoard512 panel2;
+
+	public Game512(int side) {
+		initiate(side);
+	}
+
+	private void initiate(int side) {
+
+		this.setLayout(new GridLayout(1, 2, 50, 50));
+		panel1 = new GameBoard512(side, this);
+		panel2 = new GameBoard512(side, this);
+		this.add(panel1);
+		this.add(panel2);
+
+		panel1.setKeyListenerMode(false);
+		panel2.setKeyListenerMode(true);
+
+	}
+
+	public GameBoard getGameBoard1() {
+		return panel1;
+	}
+
+	public GameBoard getGameBoard2() {
+		return panel2;
+	}
+
+	void gameOver(GameBoard512 gb, boolean lose) throws Exception {
+		if (lose && panel1 == gb || !lose && panel2 == gb)
+			Game2048.game.showController(Game2048.PLAYERTWOWINS);
+		else
+			Game2048.game.showController(Game2048.PLAYERONEWINS);
+	}
+
+}
---
+++ src/com/wordpress/game2048/GameOver512.java
@@ -0,0 +1,61 @@
+package com.wordpress.game2048;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by scott on 10/23/16.
+ */
+class GameOver512 extends JPanel { // multi-player game over
+
+	private JButton tryAgain = new JButton("Try Again");
+	private JButton homePage = new JButton("Back to Home");
+
+	GameOver512(boolean winner) {
+
+		BoxLayout layout = new BoxLayout(this, BoxLayout.Y_AXIS);
+		setLayout(layout);
+		setBackground(new Color(236, 196, 0));
+
+		JLabel title = new JLabel("");
+		if (winner)
+			title = new JLabel("PLAYER ONE WINS");
+		else
+			title = new JLabel("PLAYER TWO WINS");
+		title.setFont(new Font("Arial", Font.BOLD, 60));
+		title.setForeground(Color.RED);
+
+		tryAgain.setFont(new Font("Arial", Font.PLAIN, 20));
+		homePage.setFont(new Font("Arial", Font.PLAIN, 20));
+
+		add(title);
+		add(Box.createRigidArea(new Dimension(0, 20)));
+		add(tryAgain);
+		add(Box.createRigidArea(new Dimension(0, 20)));
+		add(homePage);
+
+		title.setAlignmentX(Component.CENTER_ALIGNMENT);
+		tryAgain.setAlignmentX(Component.CENTER_ALIGNMENT);
+		homePage.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+		tryAgain.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Game2048.game.showGameBoard(Game2048.game.gameMode);
+			}
+
+		});
+
+		homePage.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					Game2048.game.showController(Game2048.HOMEPAGE);
+				} catch (Exception ex) {
+					System.out.println("TROLOLOLOLOL");
+				}
+			}
+
+		});
+	}
+}
